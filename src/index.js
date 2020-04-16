d22f94253b8693a304d4f5013b836a04a625ccd0
Kyryl Dzhosan <kirildz@protonmail.com>
2020-04-16 13:27:19 +0300

    minor

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import { congratMassage, gameDescription, gameDefinition } from './helper.js';
 const engine = (switcher) => {
   gameDescription(switcher);
   let count = 0;
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i += 1) {
     const result = gameDefinition(switcher);
     if (result) {
       console.log('Correct!');
-    }
-    if (!result) {
-      return;
+    } else {
+      break;
     }
     count += 1;
   }

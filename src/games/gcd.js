1ccb3954aedf552f877b3b372ff42b57144ef033
kirildz <kirildz@protonmail.com>
2020-04-22 04:47:38 +0300

    minor

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,26 @@
 import { generator } from '../helper.js';
 import engine from '../index.js';
 
+const gcdChecker = (num1, num2) => {
+  const smallerNum = num1 > num2 ? num2 : num1;
+  let result = 0;
+  for (let j = smallerNum; j >= 1; j -= 1) {
+    if (num1 % j === 0 && num2 % j === 0) {
+      result = j;
+    }
+  }
+  return result;
+};
+
 const gcd = () => {
   const gameData = [[], []];
   for (let i = 0; i < 3; i += 1) {
     const num1 = generator(1, 100);
     const num2 = generator(1, 100);
-    const smallerNum = num1 > num2 ? num2 : num1;
     gameData[0].push(`${num1} ${num2}`);
-    for (let j = smallerNum; j >= 1; j -= 1) {
-      if (num1 % j === 0 && num2 % j === 0) {
-        gameData[1].push(j);
-        break;
-      }
-    }
+    const result = gcdChecker(num1, num2);
+    gameData[1].push(result);
   }
-
   return gameData;
 };
 

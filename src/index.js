aa06c82db5c9e74b9a0829a4472751c5cf31bdc7
kirildz <kirildz@protonmail.com>
2020-07-02 18:05:36 +0300

    Minor

--- src/index.js
+++ src/index.js
@@ -3,22 +3,28 @@ import { car, cdr } from './helper.js';
 
 export const questionCounter = 3;
 
-const userCommunication = readlineSync.question;
-const userName = userCommunication('Welcome to the Brain Games!\nMay I have your name? ');
-
 export const engine = (gameData, startMassage) => {
+
+  const userCommunication = readlineSync.question;
+  const userName = userCommunication('Welcome to the Brain Games!\nMay I have your name? ');
+
   console.log(startMassage);
+
   for (let i = 0; i < questionCounter; i += 1) {
     const question = `Question: ${car(gameData[i])}`;
     console.log(question);
     const userAnswer = userCommunication('Your answer: ');
+
     if (userAnswer === `${cdr(gameData[i])}`) {
       console.log('Correct!');
     }
+
     if (userAnswer !== `${cdr(gameData[i])}`) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${cdr(gameData[i])}".\n Let's try again, ${userName}!`);
       return;
     }
+
   }
+
   console.log(`Congratulations, ${userName}!`);
 };
